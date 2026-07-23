9f84fbfab50c1c809c33336e5fdb6683c53e0fcb
Kevin <kevin.lhuillier@outlook.fr>
2026-07-23 19:23:24 +0200

    Change directories

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
 
         try {
             // Appel à l'API
-            const res = await fetch("/api/login", {
+            const res = await fetch("/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -31,7 +31,7 @@ export default function RegisterPage() {
         setIsLoading(true)
 
         try {
-            const res = await fetch("/api/register", {
+            const res = await fetch("/api/auth/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name, email, password }),
